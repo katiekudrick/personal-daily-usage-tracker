12ea154d9a48d6af565594219e7009ea0904af34
Katie Kudrick <khkudrick@gmail.com>
2022-12-29 17:10:04 -0500

    git commit -m "Updated changes, added Flask, changed usage tracking to date and time instead of quanity usage, added HTML template rendering, added MySQL database for usage"

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 // Array to store item data
 const items = [];
 
@@ -38,21 +40,20 @@ function addItem() {
 
 // Log Usage Form
 function logUsage() {
-  // Get the selected item name
-  const itemName = $("#item-name").val();
+  // Get the selected item name from the dropdown menu
+  const itemName = document.getElementById("item-name").value;
 
-  // Find the item in the array
-  const item = items.find((i) => i.name === itemName);
+  // Get the usage date and time from the form inputs
+  const usageDate = document.getElementById("item-usage-date").value;
+  const usageTime = document.getElementById("item-usage-time").value;
 
-  // Get the usage from the form input
-  const usage = $("#item-usage").val();
+  // Combine the date and time into a single string
+  const usageDateTime = `${usageDate} ${usageTime}`;
 
-  // Update the item's usage and cost per use
-  item.usage += usage;
-  item.costPerUse = item.totalCost / item.usage;
-
-  // Update the table row with the new data
-  updateItemInTable(item);
+  // Record the usage in the database
+  // You will need to implement this part yourself
+  // depending on how you are storing the usage data
+  recordUsage(itemName, usageDateTime);
 }
 
 // Add an item to the table
@@ -82,3 +83,72 @@ function updateItemInTable(item) {
     <td>${item.usage}</td>
     <td>${item.costPerUse}</td>`);
 }
+
+// Creating usage database
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "your-username",
+  password: "your-password",
+  database: "your-database",
+});
+
+function recordUsage(itemName, usageDateTime) {
+  connection.query(
+    "INSERT INTO usage (item_name, usage_date_time) VALUES (?, ?)",
+    [itemName, usageDateTime],
+    function (error, results, fields) {
+      if (error) throw error;
+    }
+  );
+}
+
+// Connect to Python backend with HTTP request
+
+// Request data from the '/data' route on the server
+axios.get('http://localhost:3000/data')
+  .then(response => {
+    // Do something with the response data
+  })
+  .catch(error => {
+    // Handle the error
+  });
+
+// Request data from the '/users' route with a query parameter
+axios.get('http://localhost:3000/users?id=123')
+  .then(response => {
+    // Do something with the response data
+  })
+  .catch(error => {
+    // Handle the error
+  });
+
+// Send a POST request with data to the '/users' route
+axios.post('http://localhost:3000/users', {
+  name: 'John',
+  age: 30
+})
+  .then(response => {
+    // Do something with the response data
+  })
+  .catch(error => {
+    // Handle the error
+  });
+
+// handle the response data and any errors that might occur during the request:
+// Request data from the '/data' route on the server
+axios.get('http://localhost:3000/data')
+  .then(response => {
+    // Parse the response data
+    const data = response.data;
+
+    // Display the data on the page
+    const output = document.getElementById('output');
+    output.innerHTML = data;
+  })
+  .catch(error => {
+    // Display an error message to the user
+    const output = document.getElementById('output');
+    output.innerHTML = 'An error occurred';
+  });
